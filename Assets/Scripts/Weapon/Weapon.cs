04b21f20626625a51ed9d4389fbd1861d7f7b292
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-11 22:05:29 +0900

    무기에 관한 데이터를 담아줄 클래스

---
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon
+{
+    // 추후에 테이블에서 로드하도록 변경
+    public int punch_attack_damage;
+    public float punch_attack_distance;
+    public float punch_attack_angle;
+
+    public int combat_attack_damage;
+    public float combat_attack_distance;
+    public float combat_attack_angle;
+
+    public int spider_attack_damage;
+    public float spider_attack_distance;
+    public float spider_attack_angle;
+}
