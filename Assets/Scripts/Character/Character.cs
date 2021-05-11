84247298644f49d8650c0a122d151f5fa37a6231
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-11 22:08:51 +0900

    캐릭터, NPC의 데이터를 담아줄 클래스

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    public int HP;
+    public int attack_damage;
+    public float attack_distance;
+    public float attack_angle;
+}
