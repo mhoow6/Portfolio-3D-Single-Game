9263e27cf870aa6fb063a4fdd9f90d5d74308b21
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-12 22:08:16 +0900

    무기 정보 테이블에 있는 필드명대로 멤버변수 변경

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -2,18 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon
+public struct Weapon
 {
-    // 추후에 테이블에서 로드하도록 변경
-    public int punch_attack_damage;
-    public float punch_attack_distance;
-    public float punch_attack_angle;
-
-    public int combat_attack_damage;
-    public float combat_attack_distance;
-    public float combat_attack_angle;
-
-    public int spider_attack_damage;
-    public float spider_attack_distance;
-    public float spider_attack_angle;
+    public ushort id;
+    public string weapon_name;
+    public float basic_damage;
+    public float basic_distance;
+    public float basic_angle;
 }
