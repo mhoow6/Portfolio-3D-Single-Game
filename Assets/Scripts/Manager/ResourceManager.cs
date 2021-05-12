f0cb4cb8e03de56fb2aad8fdf1e4930e489aff0b
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-12 22:10:34 +0900

    - 몬스터 Transform 정보 테이블에서 Transform 데이터를 가져온 후, 몬스터 아이디에 맞는  스크립트 추가 - 무기 정보 테이블로부터 데이터를 가져온 후, 리소스 매니저의 weapons에 추가

--- Assets/Scripts/Manager/ResourceManager.cs
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-// 캐릭터 테이블에서 HP 로드 -> 각 캐릭터에게 제공
-// 무기 테이블에서 무기 공격력, 범위각, 공격거리 로드 -> 각 무기에게 제공
-
 // 주된 역할: 게임 실행시 씬, 몬스터, 캐릭터 배치
-
 public class ResourceManager : MonoBehaviour
 {
     public static ResourceManager instance;
-    public Weapon weapons;
-    public List<Monster> forestMonsters;
+
+    // WeaponInfoTableManager로 옮길 것
+    public List<Weapon> weapons;
 
     private string forestMonsterPath;
     private string weaponPath;
+    private string playerPath;
+    private string npcPath;
+
+    private void Awake()
+    {
+        instance = this;
+        weapons = new List<Weapon>();
+        forestMonsterPath = Application.dataPath + "/Resources/Tables/ForestMonsterPosition.csv";
+        weaponPath = Application.dataPath + "/Resources/Tables/WeaponInfo.csv";
+
+        ResourceFromWeaponTable(weaponPath);
+        // CreateMonster(forestMonsterPath);
+    }
 
     private void CreateMonster(string fileName)
     {
@@ -24,28 +34,44 @@ public class ResourceManager : MonoBehaviour
             string line = string.Empty;
             GameObject parent = new GameObject("Monster");
 
+            sr.ReadLine(); // 첫 레코드 스킵
+
             while ((line = sr.ReadLine()) != null)
             {
                 string[] datas = line.Split(',');
 
-                string objName = datas[0];
-                float xPos = float.Parse(datas[1]);
-                float zPos = float.Parse(datas[2]);
+                ushort index = ushort.Parse(datas[0]);
+                ushort id = ushort.Parse(datas[1]);
+                float xPos = float.Parse(datas[2]);
+                float yPos = float.Parse(datas[3]);
+                float zPos = float.Parse(datas[4]);
+                float xRot = float.Parse(datas[5]);
+                float yRot = float.Parse(datas[6]);
+                float zRot = float.Parse(datas[7]);
+                float xScale = float.Parse(datas[8]);
+                float yScale = float.Parse(datas[9]);
+                float zScale = float.Parse(datas[10]);
 
-                GameObject _obj = Resources.Load<GameObject>("Character/Monster/" + objName);
+                string mobName = GetMonsterNametoID(id);
+
+                GameObject _obj = Resources.Load<GameObject>("Character/Monster/" + mobName);
                 GameObject obj = Instantiate(_obj);
+                Monster monster = Monster.AddMonsterComponent(obj, id);
 
-                Monster monster = Monster.AddMonsterComponent(obj, objName);
-                monster.name = objName;
+                monster.index = index;
+                monster.id = id;
+                monster.name = mobName;
 
-                monster.transform.position = Utility.RayToDown(new Vector3(xPos, 0, zPos));
-                forestMonsters.Add(monster);
+                monster.transform.position = Utility.RayToDown(new Vector3(xPos, yPos, zPos)); // 떠 있는 현상 방지
+                monster.transform.rotation = Quaternion.Euler(new Vector3(xRot, yRot, zRot));
+                monster.transform.localScale = new Vector3(xScale, yScale, zScale);
 
                 monster.transform.SetParent(parent.transform);
             }
         }
     }
 
+    // WeaponInfoTableManager로 옮길 것
     private void ResourceFromWeaponTable(string fileName)
     {
         using (StreamReader sr = new StreamReader(fileName))
@@ -56,53 +82,48 @@ public class ResourceManager : MonoBehaviour
             {
                 string[] datas = line.Split(',');
 
-                switch (datas[0])
-                {
-                    case "punch_attack":
-                        weapons.punch_attack_damage = int.Parse(datas[1]);
-                        weapons.punch_attack_distance = float.Parse(datas[2]);
-                        weapons.punch_attack_angle = float.Parse(datas[3]);
-                        break;
+                if (datas[0] == "id")
+                    continue;
 
-                    case "combat_attack":
-                        weapons.combat_attack_damage = int.Parse(datas[1]);
-                        weapons.combat_attack_distance = float.Parse(datas[2]);
-                        weapons.combat_attack_angle = float.Parse(datas[3]);
-                        break;
+                Weapon weapon = new Weapon();
+                weapon.id = ushort.Parse(datas[0]);
+                weapon.weapon_name = datas[1];
+                weapon.basic_damage = float.Parse(datas[2]);
+                weapon.basic_distance = float.Parse(datas[3]);
+                weapon.basic_angle = float.Parse(datas[4]);
 
-                    case "spider_attack":
-                        weapons.spider_attack_damage = int.Parse(datas[1]);
-                        weapons.spider_attack_distance = float.Parse(datas[2]);
-                        weapons.spider_attack_angle = float.Parse(datas[3]);
-                        break;
-                }
+                weapons.Add(weapon);
             }
         }
     }
 
-    private void Awake()
-    {
-        instance = this;
-        weapons = new Weapon();
-        forestMonsterPath = Application.dataPath + "/Resources/Tables/ForestMonster.csv";
-        weaponPath = Application.dataPath + "/Resources/Tables/Weapon.csv";
-
-        ResourceFromWeaponTable(weaponPath);
-        CreateMonster(forestMonsterPath);
-    }
-
     private void CheckWeaponData()
     {
-        Debug.Log(weapons.punch_attack_damage);
-        Debug.Log(weapons.punch_attack_distance);
-        Debug.Log(weapons.punch_attack_angle);
+        Debug.Log(weapons[0].id);
+    }
 
-        Debug.Log(weapons.combat_attack_damage);
-        Debug.Log(weapons.combat_attack_distance);
-        Debug.Log(weapons.combat_attack_angle);
+    public Weapon GetWeaponFromWeaponID(ushort weaponID)
+    {
+        foreach (Weapon weapon in weapons)
+        {
+            if (weapon.id == weaponID)
+                return weapon;
+        }
+        throw new System.NotSupportedException("무기중에" + weaponID + " 은 없습니다.");
+    }
 
-        Debug.Log(weapons.spider_attack_damage);
-        Debug.Log(weapons.spider_attack_distance);
-        Debug.Log(weapons.spider_attack_angle);
+    private static string GetMonsterNametoID(ushort mobID)
+    {
+        string mobInfoPath = Application.dataPath + "/Resources/Tables/MonsterInfo.csv";
+
+        MonsterInfoTableManager.LoadTable(mobInfoPath);
+
+        foreach (MonsterInfo mobinfo in MonsterInfoTableManager.mobInfoList)
+        {
+            if (mobID == mobinfo.id)
+                return mobinfo.monster_name;
+        }
+
+        throw new System.NotSupportedException(mobID + "에 해당하는 몬스터는 없습니다.");
     }
 }
