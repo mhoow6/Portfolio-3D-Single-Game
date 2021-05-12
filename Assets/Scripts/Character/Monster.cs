4fc88987a2871a032050ecd3f9a6f01216c26e05
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-12 22:16:52 +0900

    몬스터 정보 테이블로부터 멤버변수 값을 할당하도록 수정, 몬스터 아이디를 열거체로 쉽게 접근할 수 있도록 수정

--- Assets/Scripts/Character/Monster.cs
+++ Assets/Scripts/Character/Monster.cs
@@ -2,18 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum mobType
+{
+    Polygonal_Metalon_Purple = 1000,
+    Polygonal_Metalon_Green,
+    Polygonal_Metalon_Red
+}
+
 public class Monster : Character
 {
+    public ushort index;
+    public ushort id;
+    public float skill1_damage;
+    public float skill1_distance;
+    public float skill1_angle;
+
     private void Awake()
     {
-        HP = 150; // 임시
+        hp = 100; // 임시
     }
 
     // 몬스터마다 죽는 방식이 다를 수 있으므로 virtual
     virtual public void Dead()
     {
-        if (HP < 0)
-            HP = 0;
+        if (hp < 0)
+            hp = 0;
 
         Invoke("Disabled", 5f);
     }
@@ -24,26 +37,26 @@ public class Monster : Character
     }
 
     // Factory Method
-    public static Monster AddMonsterComponent(GameObject obj, string objName)
+    public static Monster AddMonsterComponent(GameObject obj, ushort mobID)
     {
         Monster monster = null;
 
-        switch (objName)
+        switch (mobID)
         {
-            case "Polygonal_Metalon_Purple":
+            case (ushort)mobType.Polygonal_Metalon_Purple:
                 monster = obj.AddComponent<Spider>();
                 return monster;
 
-            case "Polygonal_Metalon_Green":
+            case (ushort)mobType.Polygonal_Metalon_Green:
                 monster = obj.AddComponent<Spider>();
                 return monster;
 
-            case "Polygonal_Metalon_Red":
+            case (ushort)mobType.Polygonal_Metalon_Red:
                 monster = obj.AddComponent<Spider>();
                 return monster;
         }
 
-        throw new System.NotSupportedException("몬스터중에" + objName + " 은 없습니다.");
+        throw new System.NotSupportedException(mobID + "에 해당하는 몬스터가 없어 스크립트 추가에 실패했습니다.");
     }
 }
 
@@ -53,14 +66,29 @@ public class CustomDummy : Monster
 
 public class NormalMob : Monster
 {
+    public float skill_1_damage;
+    public float skill_1_distance;
+    public float skill_1_angle;
 }
 
 public class Spider : NormalMob
 {
     private void Start()
     {
-        attack_damage = ResourceManager.instance.weapons.spider_attack_damage;
-        attack_distance = ResourceManager.instance.weapons.spider_attack_distance;
-        attack_angle = ResourceManager.instance.weapons.spider_attack_angle;
+        string mobInfoPath = Application.dataPath + "/Resources/Tables/MonsterInfo.csv";
+
+        MonsterInfoTableManager.LoadTable(mobInfoPath);
+
+        foreach (MonsterInfo mobinfo in MonsterInfoTableManager.mobInfoList)
+        {
+            if (mobinfo.id >= (ushort)mobType.Polygonal_Metalon_Purple && mobinfo.id < (ushort)mobType.Polygonal_Metalon_Red)
+            {
+                attack_damage = mobinfo.attack_damage;
+                attack_distance = mobinfo.attack_distance;
+                attack_angle = mobinfo.attack_angle;
+            }
+        }
+
+        GameManager.instance.monsters.Add(this);
     }
 }
