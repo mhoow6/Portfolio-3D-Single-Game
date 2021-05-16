4a38f0fe6425ea324b2885f38021a662cc6f0a31
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-16 20:33:02 +0900

    리소스매니저가 더 이상 몬스터를 배치하는 역할을 하지 않음

--- Assets/Scripts/Manager/ResourceManager.cs
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-// 주된 역할: 게임 실행시 씬, 몬스터, 캐릭터 배치
+// 테이블로 부터 데이터 로드
 public class ResourceManager : MonoBehaviour
 {
     public static ResourceManager instance;
-    private string forestMonsterPath;
-    private string testMonsterPath;
     private string weaponPath;
     private string playerPath;
     private string monsterPath;
@@ -17,8 +15,6 @@ public class ResourceManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
-        forestMonsterPath = Application.dataPath + "/Resources/Tables/ForestMonsterPosition.csv";
-        testMonsterPath = Application.dataPath + "/Resources/Tables/TestMonsterPosition.csv";
         weaponPath = Application.dataPath + "/Resources/Tables/WeaponInfo.csv";
         playerPath = Application.dataPath + "/Resources/Tables/PlayerInfo.csv";
         monsterPath = Application.dataPath + "/Resources/Tables/MonsterInfo.csv";
@@ -28,52 +24,6 @@ public class ResourceManager : MonoBehaviour
         PlayerInfoTableManager.LoadTable(playerPath);
         MonsterInfoTableManager.LoadTable(monsterPath);
         NPCInfoTableManager.LoadTable(npcPath);
-
-        CreateMonster(testMonsterPath);
-    }
-
-    private void CreateMonster(string fileName)
-    {
-        using (StreamReader sr = new StreamReader(fileName))
-        {
-            string line = string.Empty;
-            GameObject parent = new GameObject("Monster");
-
-            sr.ReadLine(); // 첫 레코드 스킵
-
-            while ((line = sr.ReadLine()) != null)
-            {
-                string[] datas = line.Split(',');
-
-                ushort index = ushort.Parse(datas[0]);
-                ushort id = ushort.Parse(datas[1]);
-                float xPos = float.Parse(datas[2]);
-                float yPos = float.Parse(datas[3]);
-                float zPos = float.Parse(datas[4]);
-                float xRot = float.Parse(datas[5]);
-                float yRot = float.Parse(datas[6]);
-                float zRot = float.Parse(datas[7]);
-                float xScale = float.Parse(datas[8]);
-                float yScale = float.Parse(datas[9]);
-                float zScale = float.Parse(datas[10]);
-
-                string mobName = MonsterInfoTableManager.GetMonsterNameFromID(id);
-
-                GameObject _obj = Resources.Load<GameObject>("Character/Monster/" + mobName);
-                GameObject obj = Instantiate(_obj);
-                Monster monster = Monster.AddMonsterComponent(obj, id);
-
-                monster.index = index;
-                monster.id = id;
-                monster.name = mobName;
-
-                monster.transform.position = Utility.RayToDown(new Vector3(xPos, yPos, zPos)); // 떠 있는 현상 방지
-                monster.transform.rotation = Quaternion.Euler(new Vector3(xRot, yRot, zRot));
-                monster.transform.localScale = new Vector3(xScale, yScale, zScale);
-
-                monster.transform.SetParent(parent.transform);
-            }
-        }
     }
 
 }
