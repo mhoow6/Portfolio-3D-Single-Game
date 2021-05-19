b0538ea636aa0959f5e5857be4d877f19fe930cd
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-19 21:59:09 +0900

    추후 NPC 타입에 따른 상호작용을 다르게 하기 위해 타입 분류 기능 추가

--- Assets/Scripts/Character/NPC.cs
+++ Assets/Scripts/Character/NPC.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class NPC : Character
 {
+    public ushort id;
+    public ushort index;
+    public byte npc_type;
+
     private void Awake()
     {
-        hp = 1;
+        hp = 0;
         attack_damage = 0;
         attack_distance = 0;
         attack_angle = 0;
+        walk_speed = 0;
+    }
+
+    private void Start()
+    {
+        foreach (NpcInfo npcinfo in NPCInfoTableManager.npcInfoList)
+        {
+            if (npcinfo.id == id)
+            {
+                npc_type = npcinfo.npc_type;
+                return;
+            }
+        }
     }
 }
 
