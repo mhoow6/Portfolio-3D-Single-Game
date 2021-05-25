c4035b8be8f1db430583133da4906140f2650960
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-25 23:40:04 +0900

    추상클래스에서 평범한 부모클래스로 변경하여, 몬스터의 기본적인 애니메이션 전환에 대한 함수 구현. 자기 자신을 가지도록 Monster self를 선언함.

--- Assets/Scripts/Animation/Monster/MonsterAnimation.cs
+++ Assets/Scripts/Animation/Monster/MonsterAnimation.cs
@@ -2,14 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-abstract public class MonsterAnimation : StateMachineBehaviour
+public class MonsterAnimation : StateMachineBehaviour
 {
-    protected float prevHP;
-    abstract protected void SwitchIdle(Animator animator);
-    abstract protected void SwitchWalk(Animator animator);
-    abstract protected void SwitchRun(Animator animator);
-    abstract protected void SwitchInjured(Animator animator, float prevHP);
-    abstract protected void SwitchDead(Animator animator);
-    abstract protected void SwitchAttack(Animator animator);
+    public enum BasicAniType
+    {
+        IDLE = 0,
+        WALK,
+        RUN,
+        INJURED,
+        DEAD,
+        ATTACK
+    }
 
+    protected float prevHP;
+    protected Monster self;
+
+    protected void IdleCondition(Animator animator, Monster monster)
+    {
+        if (monster.thinking_param == (int)BasicAniType.IDLE)
+            animator.SetInteger("ani_id", (int)BasicAniType.IDLE);
+    }
+
+    protected void WalkCondition(Animator animator, Monster monster)
+    {
+        if (monster.thinking_param == (int)BasicAniType.WALK)
+            animator.SetInteger("ani_id", (int)BasicAniType.WALK);
+    }
+
+    protected void RunCondition(Animator animator, Monster monster)
+    {
+        if (monster.thinking_param == (int)BasicAniType.RUN)
+            animator.SetInteger("ani_id", (int)BasicAniType.RUN);
+    }
+
+    protected void InjuredCondition(Animator animator, float prevHP, Monster monster)
+    {
+        if (monster.hp < prevHP && animator.GetComponent<Monster>().hp > 0)
+            animator.SetInteger("ani_id", (int)BasicAniType.INJURED);
+    }
+
+    protected void DeadCondition(Animator animator, Monster monster)
+    {
+        if (monster.hp <= 0)
+            animator.SetInteger("ani_id", (int)BasicAniType.DEAD);
+    }
+
+    protected void AttackCondition(Animator animator, Monster monster)
+    {
+        if (monster.thinking_param == (int)BasicAniType.ATTACK)
+            animator.SetInteger("ani_id", (int)BasicAniType.ATTACK);
+    }
 }
