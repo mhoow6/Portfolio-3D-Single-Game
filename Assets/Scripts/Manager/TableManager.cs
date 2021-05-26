dec56f1e7c9c74bf40df26b841e4da2405641764
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-26 22:49:51 +0900

    의미 없는 조건 비교 삭제

--- Assets/Scripts/Manager/TableManager.cs
+++ Assets/Scripts/Manager/TableManager.cs
@@ -5,7 +5,6 @@ using System.IO;
 
 public class TableManager : MonoBehaviour
 {
-    public static TableManager instance;
     private string weaponPath;
     private string playerPath;
     private string monsterPath;
@@ -13,20 +12,15 @@ public class TableManager : MonoBehaviour
 
     private void Awake()
     {
-        instance = this;
         weaponPath = Application.dataPath + "/Resources/Tables/WeaponInfo.csv";
         playerPath = Application.dataPath + "/Resources/Tables/PlayerInfo.csv";
         monsterPath = Application.dataPath + "/Resources/Tables/MonsterInfo.csv";
         npcPath = Application.dataPath + "/Resources/Tables/NPCInfo.csv";
 
-        if (!SceneInfoManager.isTableManagerAwakeOnce)
-        {
-            WeaponInfoTableManager.LoadTable(weaponPath);
-            PlayerInfoTableManager.LoadTable(playerPath);
-            MonsterInfoTableManager.LoadTable(monsterPath);
-            NPCInfoTableManager.LoadTable(npcPath);
-            SceneInfoManager.isTableManagerAwakeOnce = true;
-        }
+        WeaponInfoTableManager.LoadTable(weaponPath);
+        PlayerInfoTableManager.LoadTable(playerPath);
+        MonsterInfoTableManager.LoadTable(monsterPath);
+        NPCInfoTableManager.LoadTable(npcPath);
     }
 
 }
