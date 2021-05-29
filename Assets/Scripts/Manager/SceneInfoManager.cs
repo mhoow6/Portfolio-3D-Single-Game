403221f9e29629ef16426da28f92d3186f81a9b3
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-29 21:24:38 +0900

    제네릭 싱글톤을 상속받게 됨.

--- Assets/Scripts/Manager/SceneInfoManager.cs
+++ Assets/Scripts/Manager/SceneInfoManager.cs
@@ -9,16 +9,16 @@ public enum SceneType
 }
 
 
-public static class SceneInfoManager
+public class SceneInfoManager : SingleTon<SceneInfoManager>
 {
-    public static SceneType beforeScene;
-    public static SceneType currentScene;
-    public static float SCENE_ROAD_DURATION = 0.5f;
-    public static float FOREST_TO_VILLAGE_MIN_X = -76.231f;
-    public static float FOREST_TO_VILLAGE_MIN_Z = -5.25178f;
-    public static float FOREST_TO_VILLAGE_MAX_Z = -2.95f;
-    public static float VILLAGE_TO_FOREST_MIN_X = 16f;
-    public static float VILLAGE_TO_FOREST_MAX_X = 17f;
-    public static float VILLAGE_TO_FOREST_MIN_Z = -0.76f;
-    public static float VILLAGE_TO_FOREST_MAX_Z = 2.067f;
+    public SceneType beforeScene;
+    public SceneType currentScene;
+    public float SCENE_ROAD_DURATION = 0.5f;
+    public float FOREST_TO_VILLAGE_MIN_X = -76.231f;
+    public float FOREST_TO_VILLAGE_MIN_Z = -5.25178f;
+    public float FOREST_TO_VILLAGE_MAX_Z = -2.95f;
+    public float VILLAGE_TO_FOREST_MIN_X = 16f;
+    public float VILLAGE_TO_FOREST_MAX_X = 17f;
+    public float VILLAGE_TO_FOREST_MIN_Z = -0.76f;
+    public float VILLAGE_TO_FOREST_MAX_Z = 2.067f;
 }
