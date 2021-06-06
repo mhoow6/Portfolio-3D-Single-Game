c2b262462f71bd927843ace7697415d315ea34b8
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-06 22:13:28 +0900

    모바일, PC 플랫폼에 맞춰 Input을 넘겨줌

---
+++ Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance;
+    public VirtualJoystick joystick;
+    public CustomCameraMoblie moblieCamera;
+
+    public Vector2 moveInput;
+    public Vector2 moveDelta;
+    public float zoomScale; // ¹Ì±¸Çö
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            StartCoroutine(MoveInputMoblie());
+            StartCoroutine(MoveDeltaMoblie());
+            return;
+        }
+
+        StartCoroutine(MoveInputPC());
+        StartCoroutine(MoveDeltaPC());
+        StartCoroutine(ShortcutPC());
+        StartCoroutine(ShortcutMoblie());
+    }
+
+    IEnumerator MoveInputPC()
+    {
+        while (true)
+        {
+            yield return null;
+            moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+    }
+
+    IEnumerator MoveInputMoblie()
+    {
+        while (true)
+        {
+            yield return null;
+
+            if (joystick != null)
+                moveInput = joystick._inputDirection;
+        }
+    }
+
+    IEnumerator MoveDeltaPC()
+    {
+        while (true)
+        {
+            yield return null;
+            moveDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        }
+    }
+
+    IEnumerator MoveDeltaMoblie()
+    {
+        while (true)
+        {
+            yield return null;
+
+            if (moblieCamera != null)
+                moveDelta = moblieCamera._moveDelta;
+        }
+    }
+
+    IEnumerator ShortcutPC()
+    {
+        while (true)
+        {
+            yield return null;
+
+            // Inventory Switch
+            if (Input.GetKeyDown(KeyCode.I))
+                HUDManager.instance.inventory.gameObject.SetActive(
+                    HUDManager.instance.inventory.isInventoryOn = HUDManager.instance.inventory.isInventoryOn == false ? true : false
+                    );
+        }
+    }
+
+    IEnumerator ShortcutMoblie()
+    {
+        while (true)
+        {
+            yield return null;
+
+            // Inventory
+            if (HUDManager.instance.menu.controlSlots.Find(slot => slot.name == "Inventory").isClicked)
+            {
+                Debug.Log("Inventory On");
+                HUDManager.instance.inventory.isInventoryOn = true;
+                HUDManager.instance.inventory.gameObject.SetActive(true);
+            }
+            
+            if (HUDManager.instance.inventory.homeBtn.isClicked && HUDManager.instance.inventory.isInventoryOn)
+            {
+                Debug.Log("Inventory Off");
+                HUDManager.instance.inventory.isInventoryOn = false;
+                HUDManager.instance.inventory.gameObject.SetActive(false);
+            }
+                
+        }
+    }
+}
