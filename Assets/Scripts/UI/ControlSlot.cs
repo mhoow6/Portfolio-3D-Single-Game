0db0e70a51d5956b10a7b73e13a5973006f42644
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-07 22:05:30 +0900

    클릭 시 알파값에 변화를 줘 클릭한 느낌을 주게 함

--- Assets/Scripts/UI/ControlSlot.cs
+++ Assets/Scripts/UI/ControlSlot.cs
@@ -2,18 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ControlSlot : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public bool isClicked;
 
+    [SerializeField]
+    private Image background;
+
+    private Color originColor;
+    private Color pressedColor;
+    private const float ALPHA_80 = 0.3137255f;
+
+    private void Awake()
+    {
+        originColor = background.color;
+        pressedColor = new Color(background.color.r, background.color.g, background.color.b, ALPHA_80);
+    }
+
     public virtual void OnPointerDown(PointerEventData eventData)
     {
         isClicked = true;
+        background.color = pressedColor;
     }
 
     public virtual void OnPointerUp(PointerEventData eventData)
     {
         isClicked = false;
+        background.color = originColor;
+    }
+
+    private void OnDisable()
+    {
+        isClicked = false;
+        background.color = originColor;
     }
 }
