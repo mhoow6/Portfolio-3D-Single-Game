e649c3729536572cbae4b75849698c18ef86c924
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-07 22:06:26 +0900

    인벤토리 카메라

---
+++ Assets/Scripts/Control/InventoryCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryCamera : MonoBehaviour
+{
+    public Vector3 BasicOffset
+    {
+        get
+        {
+            return new Vector3(1.85f, 1.5f, 0);
+        }
+    }
+}
