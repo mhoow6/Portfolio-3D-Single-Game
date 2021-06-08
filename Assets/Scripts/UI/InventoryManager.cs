0e63d2147c4ca4470bbefa6fedc32fbdfa7ca437
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-08 22:16:10 +0900

    파일 디렉토리 변경

---
+++ Assets/Scripts/UI/InventoryManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public ItemContent itemContent;
+    public TapMenu tapMenu;
+    public ControlSlot homeBtn;
+    public ControlSlot deleteBtn;
+    public InventoryCamera inventoryCamera;
+    public bool isInventoryOn;
+}
---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public List<ControlSlot> controlSlots = new List<ControlSlot>();
+}
