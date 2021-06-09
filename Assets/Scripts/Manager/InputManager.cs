d6f2a5f12697ca6e02b71909f167ac5fb969a1cd
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-09 22:07:08 +0900

    인벤토리 아이템 종류별로 볼 수 있게 탭 메뉴 구현, 이제 인벤토리에서 무기 착용을 가능함

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -30,6 +30,7 @@ public class InputManager : MonoBehaviour
         StartCoroutine(MoveDeltaPC());
         StartCoroutine(ShortcutPC());
         StartCoroutine(ShortcutMenu());
+        StartCoroutine(InventoryButton());
     }
 
     IEnumerator MoveInputPC()
@@ -116,8 +117,57 @@ public class InputManager : MonoBehaviour
                 InventorySwitch(HUDManager.instance.inventory.isInventoryOn);
             }
 
+            
+        }
+    }
+
+    IEnumerator InventoryButton()
+    {
+        while (true)
+        {
+            yield return null;
+
+            // Delete Item, Quest Item can't destroyed.
             if (HUDManager.instance.inventory.deleteBtn.isClicked)
-                ItemDelete(HUDManager.instance.inventory.itemContent.items.Find(item => item.isSelected));
+                ItemDelete(HUDManager.instance.inventory.itemContent.items.Find(item => item.isSelected && item.item_type != (byte)ItemType.QUEST));
+
+            // Tab Menu - All
+            if (HUDManager.instance.inventory.tapMenu.tabs.Find(tab => tab.isClicked && tab.gameObject.name == "All"))
+            {
+                foreach (ItemSlot item in HUDManager.instance.inventory.itemContent.items)
+                    item.gameObject.SetActive(true);
+            }
+
+            // Tab Menu - Weapon
+            if (HUDManager.instance.inventory.tapMenu.tabs.Find(tab => tab.isClicked && tab.gameObject.name == "Weapon"))
+            {
+                foreach (ItemSlot item in HUDManager.instance.inventory.itemContent.items)
+                {
+                    if (item.item_type != (byte)ItemType.EQUIPMENT)
+                        item.gameObject.SetActive(false);
+
+                    if (item.item_type == (byte)ItemType.EQUIPMENT)
+                        item.gameObject.SetActive(true);
+
+                }
+            }
+
+            // Tab Menu - Quest
+            if (HUDManager.instance.inventory.tapMenu.tabs.Find(tab => tab.isClicked && tab.gameObject.name == "Quest"))
+            {
+                foreach (ItemSlot item in HUDManager.instance.inventory.itemContent.items)
+                {
+                    if (item.item_type != (byte)ItemType.QUEST)
+                        item.gameObject.SetActive(false);
+
+                    if (item.item_type == (byte)ItemType.QUEST)
+                        item.gameObject.SetActive(true);
+                }
+            }
+
+            // Equip/UnEquip Item
+            if (HUDManager.instance.inventory.equipBtn.isClicked)
+                ItemEquip(HUDManager.instance.inventory.itemContent.items.Find(item => item.isSelected));
         }
     }
 
@@ -128,6 +178,66 @@ public class InputManager : MonoBehaviour
     }
 
     private void ItemDelete(ItemSlot item)
+    {
+        if (item != null)
+        {
+            item.itemIcon.sprite = null;
+            item.itemIcon.enabled = false;
+            item.itemCount.gameObject.SetActive(false);
+            item.isSelected = false;
+            item.itemGradeFrame.sprite = item.originGradeFrameSprite;
+            item.item_type = (byte)ItemType.NONE;
+            item.item_id = 0;
+        }
+    }
+
+    private void ItemEquip(ItemSlot item)
+    {
+        if (item != null)
+        {
+            switch (item.item_type)
+            {
+                
+                case (byte)ItemType.EQUIPMENT:
+
+                    if (HUDManager.instance.inventory.equipContent.items[(int)EquipmentIndex.WEAPON] != null)
+                    {
+                        // 1. Make EquipSlot Copy
+                        EquipSlot temp = Instantiate(HUDManager.instance.inventory.equipContent.items[(int)EquipmentIndex.WEAPON]); // Deep Copy
+                        temp.gameObject.SetActive(false);
+
+                        // 2. Inventory Item to Equip Slot
+                        HUDManager.instance.inventory.equipContent.items[(int)EquipmentIndex.WEAPON].itemIcon.sprite = item.itemIcon.sprite;
+                        HUDManager.instance.inventory.equipContent.items[(int)EquipmentIndex.WEAPON].itemGradeFrame.sprite = item.originGradeFrameSprite;
+                        HUDManager.instance.inventory.equipContent.items[(int)EquipmentIndex.WEAPON].item_id = item.item_id;
+                        HUDManager.instance.inventory.equipContent.items[(int)EquipmentIndex.WEAPON].item_type = item.item_type;
+                        HUDManager.instance.inventory.equipContent.items[(int)EquipmentIndex.WEAPON].itemCount = item.itemCount;
+
+                        // 3. Player's weapon object is Destroyed
+                        Destroy(GameManager.instance.controller.player.weapon);
+
+                        // 4. Now Player equips Changed Weapon
+                        GameManager.instance.controller.player.weapon = GameManager.instance.controller.player.GetWeaponFromResource(HUDManager.instance.inventory.equipContent.items[(int)EquipmentIndex.WEAPON].item_id);
+
+                        // 5. Then Change Player Equip id
+                        GameManager.instance.controller.player.equip_weapon_id = HUDManager.instance.inventory.equipContent.items[(int)EquipmentIndex.WEAPON].item_id;
+
+                        // 6. CopySlot To Inventory Slot
+                        item.itemIcon.sprite = temp.itemIcon.sprite;
+                        item.originGradeFrameSprite = temp.originGradeFrameSprite;
+                        item.item_id = temp.item_id;
+                        item.item_type = temp.item_type;
+                        item.itemCount = temp.itemCount;
+
+                        // 7. Destroy Temp
+                        Destroy(temp);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void ItemDelete(EquipSlot item)
     {
         if (item != null)
         {
