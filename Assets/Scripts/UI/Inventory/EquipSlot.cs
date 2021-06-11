d96aae6fd480f8350fec28cac75125a8568219d1
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-11 23:07:38 +0900

    장비 아이템 슬롯

---
+++ Assets/Scripts/UI/Inventory/EquipSlot.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class EquipSlot : ControlSlot, IPointerDownHandler
+{
+    // ItemInfo
+    public byte item_type;
+    public ushort item_id;
+    public ushort count;
+    public byte reinforce_level;
+    public string item_name;
+    // ItemInfo
+
+    public Image itemGradeFrame;
+    public Sprite originGradeFrameSprite;
+    public bool isSelected;
+    public bool isEquiped;
+
+    private const string SELECTED_FRAME = "equipment_grade_select";
+
+    private void Awake()
+    {
+        originColor = itemIcon.color;
+        originGradeFrameSprite = itemGradeFrame.sprite;
+        pressedColor = new Color(originColor.r, originColor.g, originColor.b, ALPHA_80);
+    }
+
+    public override void PointerDown(PointerEventData eventData)
+    {
+        // Turn off ItemSlot Selected
+        ItemSlot invenSelectedItem = HUDManager.instance.inventory.itemContent.items.Find(item => item.isSelected);
+        if (invenSelectedItem != null && invenSelectedItem != this)
+        {
+            invenSelectedItem.isSelected = false;
+            invenSelectedItem.itemGradeFrame.sprite = invenSelectedItem.originGradeFrameSprite;
+        }
+
+        // Prev selected item handle
+        EquipSlot selectedItem = HUDManager.instance.inventory.equipContent.items.Find(item => item.isSelected);
+        if (selectedItem != null && selectedItem != this)
+        {
+            selectedItem.isSelected = false;
+            selectedItem.itemGradeFrame.sprite = selectedItem.originGradeFrameSprite;
+        }
+
+        // Toggle Select
+        if (!isSelected && item_type != (byte)ItemType.NONE)
+            itemGradeFrame.sprite = Resources.Load<Sprite>(PlayerInventoryTableManager.spritePath + SELECTED_FRAME);
+        else
+            itemGradeFrame.sprite = originGradeFrameSprite;
+
+        isSelected = !isSelected;
+    }
+}
