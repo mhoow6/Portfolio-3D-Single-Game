87859e7a5da2d7eb31b7ac8ac315397ac597ab8a
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-11 23:06:37 +0900

    인벤토리

---
+++ Assets/Scripts/UI/Inventory/InventoryManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public ItemContent itemContent;
+    public TapMenu tapMenu;
+    public EquipContent equipContent;
+    public ControlSlot homeBtn;
+    public ControlSlot deleteBtn;
+    public ControlSlot equipBtn;
+    public InventoryCamera inventoryCamera;
+    public bool isInventoryOn;
+}
