d594cdbe9c38b02f835eb953179cf3c785a13a68
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-11 23:12:07 +0900

    테이블 매니저가 한 번이라도 Awake했는지 알 게 해주는 용도로 추가

--- Assets/Scripts/Manager/SceneInfoManager.cs
+++ Assets/Scripts/Manager/SceneInfoManager.cs
@@ -21,4 +21,5 @@ public class SceneInfoManager : SingleTon<SceneInfoManager>
     public float VILLAGE_TO_FOREST_MAX_X = 17f;
     public float VILLAGE_TO_FOREST_MIN_Z = -0.76f;
     public float VILLAGE_TO_FOREST_MAX_Z = 2.067f;
+    public bool isTableManagerAwakeOnce;
 }
