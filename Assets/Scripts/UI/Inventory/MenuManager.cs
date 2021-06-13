8467a17d0a38270c7447f32a7c408d77b7ead156
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-13 22:42:04 +0900

    메뉴 인덱스

--- Assets/Scripts/UI/Inventory/MenuManager.cs
+++ Assets/Scripts/UI/Inventory/MenuManager.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MenuIndex
+{
+    INVENTORY,
+    SYSTEM,
+    QUEST
+}
+
 public class MenuManager : MonoBehaviour
 {
     public List<ControlSlot> controlSlots = new List<ControlSlot>();
