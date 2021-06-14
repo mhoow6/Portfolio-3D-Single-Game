e27ad5b1d876172fe0d20fbd875c8a0aca381c23
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-14 23:11:03 +0900

    대화창 UI 매니저

---
+++ Assets/Scripts/UI/DialogManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogManager : MonoBehaviour
+{
+    public TMP_Text npcName;
+    public TMP_Text npcChat;
+    public Button skipBtn;
+    public RectTransform questBtnNode;
+
+
+    public void OnSkip()
+    {
+
+    }
+
+    public void OnQuestAccept()
+    {
+
+    }
+
+    public void OnQuestDecline()
+    {
+
+    }
+}
