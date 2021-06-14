773276d167da525ba3414003c8b559527312c7c5
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-14 23:12:18 +0900

    SkinnedMeshRenderer로부터 bound를 얻어오는 함수 구현, bound 충돌 시에 호출되는 함수는 팩토리 메소드를 통하여 자식 클래스에서 자유롭게 원하는 내용을 구현

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -8,4 +8,53 @@ public class Character : MonoBehaviour
     public float attack_distance;
     public float attack_angle;
     public float walk_speed;
+    public Bounds _bound
+    {
+        get => bound;
+    }
+
+    protected Bounds bound;
+
+    protected Bounds? GetBoundFromSkinnedMeshRenderer(Character character)
+    {
+        Transform trans = character.transform;
+        Bounds bound;
+
+        // 1. Get SkinnedMeshRenderer in GameObject
+        SkinnedMeshRenderer renderer = null;
+
+        // 2. Active gameobject is myself.
+        for (int i = 0; i < trans.childCount; i++)
+        {
+            if (trans.GetChild(i).gameObject.activeSelf)
+            {
+                renderer = trans.GetChild(i).GetComponent<SkinnedMeshRenderer>();
+
+                // 3. Get bounds
+                bound = renderer.bounds;
+
+                // 4. Avoid Root Object
+                return bound;
+            }
+        }
+
+        return null;
+    }
+
+    protected void BoundUpdate(Character character, bool isBoundStatic)
+    {
+        if (!isBoundStatic)
+            this.bound.center = transform.position;
+
+        if (character != null)
+        {
+            if (bound.Intersects(character._bound))
+                OnBoundEnter();
+        }
+    }
+
+    protected virtual void OnBoundEnter()
+    {
+        Debug.Log("Bound Intersects.");
+    }
 }
