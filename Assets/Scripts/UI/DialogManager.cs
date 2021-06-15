ae3094bcbd1fc41847bc8f83fe34618ad9d1a896
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-15 22:16:17 +0900

    대화창이 활성화되면 5시, 7시 방향에 있는 UI를 비활성화하여 가림. 대화창이 비활성화되면 다시 활성화

--- Assets/Scripts/UI/DialogManager.cs
+++ Assets/Scripts/UI/DialogManager.cs
@@ -10,7 +10,19 @@ public class DialogManager : MonoBehaviour
     public TMP_Text npcChat;
     public Button skipBtn;
     public RectTransform questBtnNode;
+    public bool isDialogOn;
 
+    private void OnEnable()
+    {
+        InputManager.instance.joystick.gameObject.SetActive(false);
+        HUDManager.instance.combat.gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        InputManager.instance.joystick.gameObject.SetActive(true);
+        HUDManager.instance.combat.gameObject.SetActive(true);
+    }
 
     public void OnSkip()
     {
