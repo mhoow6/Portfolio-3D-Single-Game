62701e6612d05d4e0af5e66d83b94c70aacf4f60
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-15 22:16:45 +0900

    디렉토리 변경

---
+++ Assets/Scripts/Manager/TableManager.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TableManager : MonoBehaviour
+{
+    public static TableManager instance;
+
+    [HideInInspector]
+    public string playerTempInventoryPath;
+    [HideInInspector]
+    public string playerTempEquipmentPath;
+    [HideInInspector]
+    public string playerTempPath;
+
+    private string weaponPath;
+    private string playerPath;
+    private string monsterPath;
+    private string npcPath;
+    private string playerInventoryPath;
+    private string playerEquipmentPath;
+    private string consumeItemPath;
+    private string dialogPath;
+    private string questPath;
+    private string playerQuestPath;
+    
+
+    public const string FILE_EXTENSION = ".csv";
+
+    private void Awake()
+    {
+        instance = this;
+
+        // Table Path
+        weaponPath = "Tables/WeaponInfo";
+        playerPath = "Tables/PlayerInfo";
+        monsterPath = "Tables/MonsterInfo";
+        npcPath = "Tables/NPCInfo";
+        playerInventoryPath = "Tables/PlayerInventory";
+        playerEquipmentPath = "Tables/PlayerEquipment";
+        consumeItemPath = "Tables/ConsumeItemInfo";
+        dialogPath = "Tables/DialogInfo";
+        questPath = "Tables/QuestInfo";
+        playerQuestPath = "Tables/PlayerQuest";
+
+        // Temp Path
+        playerTempInventoryPath = Application.persistentDataPath + "/Tables/PlayerInventory" + FILE_EXTENSION;
+        playerTempEquipmentPath = Application.persistentDataPath + "/Tables/PlayerEquipment" + FILE_EXTENSION;
+        playerTempPath = Application.persistentDataPath + "/Tables/PlayerInfo" + FILE_EXTENSION;
+
+        if (!SceneInfoManager.instance.isTableManagerAwakeOnce)
+        {
+            WeaponInfoTableManager.LoadTable(weaponPath);
+            PlayerInfoTableManager.LoadTable(playerPath);
+            PlayerInventoryTableManager.LoadTable(playerInventoryPath);
+            PlayerEquipmentTableManager.LoadTable(playerEquipmentPath);
+            MonsterInfoTableManager.LoadTable(monsterPath);
+            NPCInfoTableManager.LoadTable(npcPath);
+            ConsumeInfoTableManager.LoadTable(consumeItemPath);
+            DialogInfoTableManager.LoadTable(dialogPath);
+            QuestInfoTableManager.LoadTable(questPath);
+            PlayerQuestTableManager.LoadTable(playerQuestPath);
+        }
+
+        if (SceneInfoManager.instance.isTempDataExists)
+        {
+            PlayerInfoTableManager.LoadTempTable(playerTempPath);
+            PlayerInventoryTableManager.LoadTempTable(playerTempInventoryPath);
+            PlayerEquipmentTableManager.LoadTempTable(playerTempEquipmentPath);
+        }
+
+        SceneInfoManager.instance.isTableManagerAwakeOnce = true;
+    }
+
+    public List<string> GetLinesFromTable(string filePath)
+    {
+        TextAsset txtAsset = Resources.Load<TextAsset>(filePath);
+        char[] option = { '\r', '\n' };
+        string[] _lines = txtAsset.text.Split(option);
+        List<string> lines = new List<string>();
+
+        foreach (string line in _lines)
+        {
+            if (string.IsNullOrEmpty(line))
+                continue;
+
+            lines.Add(line);
+        }
+
+        return lines;
+    }
+
+    public List<string> GetLinesFromTempTable(string filePath)
+    {
+        string line = string.Empty;
+        List<string> lines = new List<string>();
+
+        using (FileStream f = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        {
+            using (StreamReader sr = new StreamReader(f, System.Text.Encoding.UTF8))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+        }
+
+        return lines;
+    }
+
+}
