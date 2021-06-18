3a70a0f018de8a37a0512db5b94c041cc39e6603
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-18 23:04:03 +0900

    퀘스트 버튼. 사실 버튼은 아니고 알림표시이다.

---
+++ Assets/Scripts/UI/QuestWindow/QuestButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestButton : MonoBehaviour
+{
+    public TMP_Text buttonText;
+    public Image selfImage;
+}
