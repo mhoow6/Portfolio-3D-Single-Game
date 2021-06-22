e03c0f3ee3d24fd17ef10eb7fd7e484ede052576
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-22 21:54:48 +0900

    인벤토리 활성화시 게임 시간 정지기능 삭제

--- Assets/Scripts/UI/Inventory/InventoryManager.cs
+++ Assets/Scripts/UI/Inventory/InventoryManager.cs
@@ -13,16 +13,23 @@ public class InventoryManager : MonoBehaviour
     public ControlSlot equipBtn;
     public InventoryCamera inventoryCamera;
     public bool isInventoryOn;
+    public Image moveItemIcon;
 
     private void OnEnable()
     {
         isInventoryOn = true;
-        HUDManager.instance.system.TimeStop();
+        //HUDManager.instance.system.TimeStop();
+
+        // Turn OFF UI
+        HUDManager.instance.inGame.gameObject.SetActive(false);
     }
 
     private void OnDisable()
     {
         isInventoryOn = false;
-        HUDManager.instance.system.TimeResume();
+        //HUDManager.instance.system.TimeResume();
+
+        // Turn ON UI
+        HUDManager.instance.inGame.gameObject.SetActive(true);
     }
 }
