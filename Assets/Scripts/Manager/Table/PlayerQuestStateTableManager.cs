7191e36aa690bcbe616480740077db4fc59634c4
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-22 21:56:21 +0900

    테이블에서 현재 플레이어 퀘스트 상태를 로드하는 로직 변경

--- Assets/Scripts/Manager/Table/PlayerQuestStateTableManager.cs
+++ Assets/Scripts/Manager/Table/PlayerQuestStateTableManager.cs
@@ -43,25 +43,13 @@ public static class PlayerQuestStateTableManager
             PlayerQuestStateInfo playerQuestInfo = new PlayerQuestStateInfo();
             string[] datas = lines[i].Split(',');
 
-            if (playerQuestStateList.Count != 0)
-            {
-                for (int j = 0; j < playerQuestStateList.Count; j++)
-                {
-                    // 테이블에서 playerQuestStateList에 있는 퀘스트 중 같은 것이 발견되면 건너뛰자.
-                    // [참고] 데이터 갱신 필요없음. 참조변수라 이미 QuestManager에서 State가 변경될 때 같이 변경됨
-                    if (ushort.Parse(datas[0]) == playerQuestStateList[j].quest_id)
-                        continue;
-                }
-            }
-            else
-            {
-                playerQuestInfo.quest_id = ushort.Parse(datas[0]);
-                playerQuestInfo.isClear = bool.Parse(datas[1]);
-                playerQuestInfo.isPlayerAccept = bool.Parse(datas[2]);
-                playerQuestInfo.target_monster_hunted = int.Parse(datas[3]);
+            playerQuestInfo.quest_id = ushort.Parse(datas[0]);
+            playerQuestInfo.isClear = bool.Parse(datas[1]);
+            playerQuestInfo.isPlayerAccept = bool.Parse(datas[2]);
+            playerQuestInfo.target_monster_hunted = int.Parse(datas[3]);
 
+            if (playerQuestStateList.Find(quest => quest.quest_id == playerQuestInfo.quest_id) == null)
                 playerQuestStateList.Add(playerQuestInfo);
-            }
         }
     }
 
