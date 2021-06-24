879136405bfb1f5e37a10f0f238abf44439f393c
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-24 21:59:56 +0900

    플레이어 사망시 나타나는 화면

---
+++ Assets/Scripts/UI/DeadWindow/DeadBanner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DeadBanner : MonoBehaviour
+{
+    public Image self;
+    public TMP_Text self_text;
+}
---
+++ Assets/Scripts/UI/DeadWindow/DeadWindowManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class DeadWindowManager : MonoBehaviour
+{
+    public Image background; // 0 ~ 180
+    public DeadBanner banner;
+    public Image goVillageBtn_image;
+    public TMP_Text goVillageBtn_text;
+    public bool isDeadWindowOn;
+
+    private Color backgroundOriginColor = new Color(0.103f, 0.081f, 0.081f, 0);
+    private Color originColor = new Color(1, 1, 1, 0);
+
+    private const float backgroundFinishAlpha = 0.7f;
+    private const float bannerFinishAlpha = 1f;
+    private const float COLOR_LERF_SPEED = 1f;
+
+    public void GoVillage()
+    {
+        SceneInfoManager.instance.beforeScene = SceneInfoManager.instance.currentScene;
+        SceneManager.LoadScene((int)SceneType.Village);
+        HUDManager.instance.system.SaveGame();
+    }
+
+    private void OnEnable()
+    {
+        isDeadWindowOn = true;
+        StartCoroutine(DeadWindowAlphaBlending());
+    }
+
+    private void OnDisable()
+    {
+        isDeadWindowOn = false;
+        background.color = backgroundOriginColor;
+        banner.self.color = originColor;
+        banner.self_text.color = originColor;
+        goVillageBtn_image.color = originColor;
+        goVillageBtn_text.color = originColor;
+    }
+
+    private IEnumerator DeadWindowAlphaBlending()
+    {
+        StartCoroutine(Utility.AlphaBlending(background, backgroundFinishAlpha, COLOR_LERF_SPEED));
+        StartCoroutine(Utility.AlphaBlending(banner.self, bannerFinishAlpha, COLOR_LERF_SPEED));
+        yield return StartCoroutine(Utility.AlphaBlending(banner.self_text, bannerFinishAlpha, COLOR_LERF_SPEED));
+        yield return StartCoroutine(ButtonAlphaBlending());
+    }
+
+    private IEnumerator ButtonAlphaBlending()
+    {
+        StartCoroutine(Utility.AlphaBlending(goVillageBtn_image, bannerFinishAlpha, COLOR_LERF_SPEED));
+        StartCoroutine(Utility.AlphaBlending(goVillageBtn_text, bannerFinishAlpha, COLOR_LERF_SPEED));
+        yield return null;
+    }
+}
