f009019a2b458959c5a7b803297eca6c8e4eddf3
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-24 22:03:13 +0900

    알파 블랜딩 효과

--- Assets/Scripts/Utility/Utility.cs
+++ Assets/Scripts/Utility/Utility.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public static class Utility
 {
@@ -24,5 +26,97 @@ public static class Utility
             }
         }
         return spawnPos;
+    }
+
+    public static IEnumerator AlphaBlending(Image origin, float finishAlpha, float lerpSpeed)
+    {
+        Color finish = new Color(origin.color.r, origin.color.g, origin.color.b, finishAlpha);
+
+        if (origin.color.a < finishAlpha)
+        {
+            while (origin.color.a < finishAlpha - 0.01f)
+            {
+                yield return null;
+
+                origin.color = Color.Lerp(origin.color, finish, Time.deltaTime * lerpSpeed);
+            }
+
+            origin.color = finish;
+        }
+
+        if (origin.color.a > finishAlpha)
+        {
+            while (origin.color.a > finishAlpha + 0.01f)
+            {
+                yield return null;
+
+                origin.color = Color.Lerp(origin.color, finish, Time.deltaTime * lerpSpeed);
+            }
+
+            origin.color = finish;
+        }
+    }
+
+    public static IEnumerator AlphaBlending(Material mat, float finishAlpha, float lerpSpeed)
+    {
+        StandardShaderUtils.ChangeRenderMode(mat, StandardShaderUtils.BlendMode.Fade);
+
+        Color finish = new Color(1, 1, 1, finishAlpha);
+        float time = 0f;
+        float end = mat.color.a <= finishAlpha ? finishAlpha - 0.1f : finishAlpha + 0.1f;
+
+        if (mat.color.a < finishAlpha)
+        {
+            while (mat.color.a < end)
+            {
+                yield return null;
+
+                time += Time.deltaTime;
+                mat.color = Color.Lerp(mat.color, finish, time * lerpSpeed);
+            }
+        }
+
+        if (mat.color.a > finishAlpha)
+        {
+            while (mat.color.a > end)
+            {
+                yield return null;
+
+                time += Time.deltaTime;
+                mat.color = Color.Lerp(mat.color, finish, time * lerpSpeed);
+            }
+        }
+
+        mat.color = finish;
+        StandardShaderUtils.ChangeRenderMode(mat, StandardShaderUtils.BlendMode.Opaque);
+    }
+
+    public static IEnumerator AlphaBlending(TMP_Text tmpText, float finishAlpha, float lerpSpeed)
+    {
+        Color finish = new Color(tmpText.color.r, tmpText.color.g, tmpText.color.b, finishAlpha);
+
+        if (tmpText.color.a < finishAlpha)
+        {
+            while (tmpText.color.a < finishAlpha - 0.01f)
+            {
+                yield return null;
+
+                tmpText.color = Color.Lerp(tmpText.color, finish, Time.deltaTime * lerpSpeed);
+            }
+
+            tmpText.color = finish;
+        }
+
+        if (tmpText.color.a > finishAlpha)
+        {
+            while (tmpText.color.a > finishAlpha + 0.01f)
+            {
+                yield return null;
+
+                tmpText.color = Color.Lerp(tmpText.color, finish, Time.deltaTime * lerpSpeed);
+            }
+
+            tmpText.color = finish;
+        }
     }
 }
