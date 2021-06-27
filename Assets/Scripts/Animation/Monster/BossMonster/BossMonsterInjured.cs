ee2db954cb8a61f205506a573a543334c854f066
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-27 22:41:52 +0900

    스턴 쿨타임을 지나게 하기 위해 코루틴 실행 (현재 공격 코루틴과 엉키는 버그 존재)

--- Assets/Scripts/Animation/Monster/BossMonster/BossMonsterInjured.cs
+++ Assets/Scripts/Animation/Monster/BossMonster/BossMonsterInjured.cs
@@ -7,20 +7,15 @@ public class BossMonsterInjured : BossMonsterAnimation
     protected override void StateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         self.endurance_stack = 0;
+        self.isStuned = true;
         animationHandler = IdleCondition;
         animationHandler += InjuredCondition;
         animationHandler += DeadCondition;
     }
 
-    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animationHandler(animator, self);
-    }
-
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         self.thinking_param = (int)AniType.IDLE;
-        self.isStuned = true;
-        self.Stun();
+        self.StartCoroutine(self.StunCooldown(self.stun_escape));
     }
 }
--- Assets/Scripts/Animation/Monster/EliteMonster/EliteMonsterInjured.cs
+++ Assets/Scripts/Animation/Monster/EliteMonster/EliteMonsterInjured.cs
@@ -7,16 +7,15 @@ public class EliteMonsterInjured : EliteMonsterAnimation
     protected override void StateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         self.endurance_stack = 0;
+        self.isStuned = true;
         animationHandler = IdleCondition;
         animationHandler += InjuredCondition;
         animationHandler += DeadCondition;
-        damagedStateHandler = null;
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         self.thinking_param = (int)AniType.IDLE;
-        self.isStuned = true;
-        self.Stun();
+        self.StartCoroutine(self.StunCooldown(self.stun_escape));
     }
 }
