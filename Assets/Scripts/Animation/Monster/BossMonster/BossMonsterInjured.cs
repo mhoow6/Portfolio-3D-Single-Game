5085e3bae843c11ed988be6803f77d559a66fb2c
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-28 21:59:40 +0900

    스턴 애니메이션 끝날 시 부울 값을 던져줘 몬스터의 생각을 다시하게 못하게 한다.

--- Assets/Scripts/Animation/Monster/BossMonster/BossMonsterInjured.cs
+++ Assets/Scripts/Animation/Monster/BossMonster/BossMonsterInjured.cs
@@ -7,15 +7,19 @@ public class BossMonsterInjured : BossMonsterAnimation
     protected override void StateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         self.endurance_stack = 0;
-        self.isStuned = true;
         animationHandler = IdleCondition;
         animationHandler += InjuredCondition;
         animationHandler += DeadCondition;
     }
 
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    protected override void StateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         self.thinking_param = (int)AniType.IDLE;
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        self.isStuned = true;
         self.StartCoroutine(self.StunCooldown(self.stun_escape));
     }
 }
