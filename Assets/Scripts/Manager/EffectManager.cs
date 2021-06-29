7573d5b2d5af1417a6f9ca74abaf879891ae5ad8
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-06-29 22:04:25 +0900

    공격 이펙트는 로컬 위치 값을 변경, 피격 이펙트 함수 구현. 이제 로컬 포지션에 들어가게끔 하면 됌.

--- Assets/Scripts/Manager/EffectManager.cs
+++ Assets/Scripts/Manager/EffectManager.cs
@@ -11,18 +11,34 @@ public class EffectManager : MonoBehaviour
     private Vector3 POLYTOPE_STUDIO_CHARACTER_STUN_EFFECT_ROT = new Vector3(0, 0, 24.618f);
     private Vector3 POLYGONAL_METALON_SPIDER_STUN_EFFECT_POS = new Vector3(-0.358f, -0.75f, 0);
     private Vector3 POLYGONAL_METALON_SPIDER_STUN_EFFECT_ROT = Vector3.zero;
-    private Vector3 FX_NORMAL_COMBAT_ATTACK_01_POS = new Vector3(0.97f, 1.507f, -0.304f);
-    private Vector3 FX_NORMAL_COMBAT_ATTACK_01_ROT = new Vector3(-34.76f, 128.518f, 35.615f);
+    private Vector3 FX_NORMAL_COMBAT_ATTACK_01_POS = new Vector3(0.3039f, 1.007f, 0.9700f);
+    private Vector3 FX_NORMAL_COMBAT_ATTACK_01_ROT = new Vector3(-34.76f, 38.518f, 35.615f);
+    private Vector3 FX_NORMAL_COMBAT_ATTACK_02_POS = new Vector3(0.3039f, 1.007f, 0.9700f);
+    private Vector3 FX_NORMAL_COMBAT_ATTACK_02_ROT = new Vector3(-13.6f, 48.598f, 11.714f);
+    private Vector3 FX_NORMAL_COMBAT_ATTACK_03_POS = new Vector3(0.6199f, 1.02f, 0.9700f);
+    private Vector3 FX_NORMAL_COMBAT_ATTACK_03_ROT = new Vector3(17.899f, 47.508f, -15.723f);
+
+    private const string attackEffectName = "FX_NormalAttack";
+    private ushort attackEffectID;
+    private const string starStunEffectName = "FX_StarStunned_01";
+    private ushort starStunEffectID;
+    private const string hitEffectName = "FX_NormalHit";
+    private ushort hitEffectID;
 
     private void Awake()
     {
         instance = this;
     }
 
+    private void Start()
+    {
+        attackEffectID = EffectInfoTableManager.GetEffectIDFromPrefabName(attackEffectName);
+        starStunEffectID = EffectInfoTableManager.GetEffectIDFromPrefabName(starStunEffectName);
+        hitEffectID = EffectInfoTableManager.GetEffectIDFromPrefabName(hitEffectName);
+    }
+
     public Effect CreateStarStunEffect(ushort objID, Transform head)
     {
-        string starStunEffectName = "FX_StarStunned_01";
-        ushort starStunEffectID = EffectInfoTableManager.GetEffectIDFromPrefabName(starStunEffectName);
 
         // 1. 이펙트 풀에 이미 해당 위치에 비활성화 된 이펙트가 있는지 검사
         Effect existEffect = effects.Find(eff => eff.effectNode == head && eff.effectID == starStunEffectID && !eff.ps.isPlaying);
@@ -59,51 +75,105 @@ public class EffectManager : MonoBehaviour
         return effect;
     }
 
-    public Effect CreateNormalAttackEffect(int aniIndex)
+    public PlayerAttackEffect CreateAttackEffect(int aniIndex)
     {
-        string normalAttackEffectName = "FX_NormalAttack";
-        ushort normalAttackEffectID = EffectInfoTableManager.GetEffectIDFromPrefabName(normalAttackEffectName);
-
         // 이펙트 풀에 이미 해당 노드에 비활성화 된 이펙트가 있는지 검사
-        Effect existEffect = effects.Find(eff => eff.effectNode == this.transform && eff.effectID == normalAttackEffectID && !eff.ps.isPlaying);
+        PlayerAttackEffect existEffect = (PlayerAttackEffect)effects.Find(eff => eff.effectID == attackEffectID && !eff.ps.isPlaying);
 
         if (existEffect != null)
         {
             // 해당 공격에 맞게 이펙트 조정
-            switch (aniIndex)
-            {
-                case (int)PlayerAnimation.AniType.COMBAT_ATTACK_01:
-                    existEffect.transform.position = GameManager.instance.controller.player.transform.position + FX_NORMAL_COMBAT_ATTACK_01_POS;
-                    existEffect.transform.rotation = Quaternion.Euler(FX_NORMAL_COMBAT_ATTACK_01_ROT);
-                    break;
-            }
-
+            AttackEffectTransformSetup(existEffect, aniIndex);
             return existEffect;
         }
-            
+
         // 그게 아니면 새로 이펙트 생성
-        GameObject _effect = Resources.Load<GameObject>("Particle/" + normalAttackEffectName);
-        Effect effect = Instantiate(_effect.AddComponent<PlayerAttackEffect>());
+        GameObject _effect = Resources.Load<GameObject>("Particle/" + attackEffectName);
+        GameObject effect = Instantiate<GameObject>(_effect);
 
-        // 데이터 추가
-        effect.effectID = normalAttackEffectID;
-        effect.effectNode = this.transform;
-
+        PlayerAttackEffect eff = effect.AddComponent<PlayerAttackEffect>();
+        
         // 새로 생긴 이펙트를 이펙트 풀에 추가 및 부모 지정
-        effects.Add(effect);
-        effect.transform.SetParent(this.transform);
+        effects.Add(eff);
+        effect.transform.SetParent(GameManager.instance.controller.player.transform);
 
         // 해당 공격에 맞게 이펙트 조정
+        AttackEffectTransformSetup(eff, aniIndex);
+
+        // 해당 이펙트 return
+        return eff;
+    }
+
+    public PlayerAttackHitEffect CreateHitEffect(Monster hitMob)
+    {
+        // 이펙트 풀에 이미 해당 노드에 비활성화 된 이펙트가 있는지 검사
+        PlayerAttackHitEffect existEffect = (PlayerAttackHitEffect)effects.Find(eff => eff.effectID == attackEffectID && eff.effectNode == hitMob.transform && !eff.ps.isPlaying);
+
+        if (existEffect != null)
+        {
+            // 해당 공격에 맞게 이펙트 위치 조정
+            existEffect.transform.position = HitEffectPositionSetup(hitMob, GameManager.instance.controller.player.transform);
+            return existEffect;
+        }
+
+        // 그게 아니면 새로 이펙트 생성
+        GameObject _effect = Resources.Load<GameObject>("Particle/" + attackEffectName);
+        GameObject effect = Instantiate<GameObject>(_effect);
+
+        PlayerAttackHitEffect eff = effect.AddComponent<PlayerAttackHitEffect>();
+
+        // 새로 생긴 이펙트를 이펙트 풀에 추가 및 부모 지정
+        effects.Add(eff);
+        effect.transform.SetParent(GameManager.instance.controller.player.transform);
+
+        // 해당 공격에 맞게 이펙트 조정
+        effect.transform.position = HitEffectPositionSetup(hitMob, GameManager.instance.controller.player.transform);
+
+        // 해당 이펙트 return
+        return eff;
+    }
+
+    private void AttackEffectTransformSetup(Effect eff, int aniIndex)
+    {
+        eff.effectID = attackEffectID;
+        eff.effectNode = GameManager.instance.controller.player.transform;
+
         switch (aniIndex)
         {
             case (int)PlayerAnimation.AniType.COMBAT_ATTACK_01:
-                effect.transform.position = GameManager.instance.controller.player.transform.position + FX_NORMAL_COMBAT_ATTACK_01_POS;
-                effect.transform.rotation = Quaternion.Euler(FX_NORMAL_COMBAT_ATTACK_01_ROT);
+                eff.transform.localPosition = FX_NORMAL_COMBAT_ATTACK_01_POS;
+                eff.transform.localRotation = Quaternion.Euler(FX_NORMAL_COMBAT_ATTACK_01_ROT);
+                break;
+
+            case (int)PlayerAnimation.AniType.COMBAT_ATTACK_02:
+                eff.transform.localPosition = FX_NORMAL_COMBAT_ATTACK_02_POS;
+                eff.transform.localRotation = Quaternion.Euler(FX_NORMAL_COMBAT_ATTACK_02_ROT);
+                break;
+
+            case (int)PlayerAnimation.AniType.COMBAT_ATTACK_03:
+                eff.transform.localPosition = FX_NORMAL_COMBAT_ATTACK_03_POS;
+                eff.transform.localRotation = Quaternion.Euler(FX_NORMAL_COMBAT_ATTACK_03_ROT);
                 break;
         }
-        
-        // 해당 이펙트 return
-        return effect;
+    }
+
+    private Vector3 HitEffectPositionSetup(Monster hitMob, Transform player)
+    {
+        float minDistance = Vector3.Distance(player.position, hitMob.hitEffectsPos[0].position);
+        Vector3 effectPos = hitMob.hitEffectsPos[0].position;
+
+        for (int i = 1; i < hitMob.hitEffectsPos.Length; i++)
+        {
+            float dis = Vector3.Distance(player.position, hitMob.hitEffectsPos[i].position);
+
+            if (dis < minDistance)
+            {
+                minDistance = dis;
+                effectPos = hitMob.hitEffectsPos[i].position;
+            }
+        }
+
+        return effectPos;
     }
 
 }
