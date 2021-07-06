7476beab5890ed798feef10c19be3c31847e15d5
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-07-06 22:20:58 +0900

    컴바인한 매쉬는 버텍스 수가 65535를 넘지 못함. 그에 따른 예외처리

--- Assets/Scripts/Utility/MeshCombine.cs
+++ Assets/Scripts/Utility/MeshCombine.cs
@@ -13,6 +13,7 @@ public class MeshCombine : MonoBehaviour
     
     MeshFilter[] meshFilters;
     int childCount;
+    const int MAX_LIMIT_VERTEX = 65535;
 
     void Start()
     {
@@ -33,17 +34,27 @@ public class MeshCombine : MonoBehaviour
         GetChildrenMeshFilter();
 
         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+        int totalVertex = 0;
 
         int i = 0;
         while (i < meshFilters.Length)
         {
             combine[i].mesh = meshFilters[i].sharedMesh;
             combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            totalVertex += meshFilters[i].sharedMesh.vertexCount;
             meshFilters[i].gameObject.SetActive(false);
 
             i++;
         }
+
         selfMeshFilter.sharedMesh = new Mesh();
+
+        if (totalVertex > MAX_LIMIT_VERTEX)
+        {
+            Clean();
+            throw new System.Exception(gameObject.name + " 의 컴바인한 매쉬의 버텍스가 " + MAX_LIMIT_VERTEX + "을 넘습니다. \"" + (totalVertex - MAX_LIMIT_VERTEX) + "\" 개의 버텍스를 줄여주세요.");
+        }
+        
         selfMeshFilter.sharedMesh.CombineMeshes(combine);
         transform.gameObject.SetActive(true);
     }
@@ -61,7 +72,9 @@ public class MeshCombine : MonoBehaviour
     {
         meshFilters = null;
         childCount = 0;
+        selfMeshFilter.sharedMesh = null;
         selfMeshFilter = null;
+        
 
         for (int i = 0; i < transform.childCount; i++)
         {
