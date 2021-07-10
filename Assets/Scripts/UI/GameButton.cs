ff6d04bea5b525dabc59ad237f9ea551bca3eac3
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-07-10 23:39:36 +0900

    타이틀에서 게임시작 시 게임버튼

---
+++ Assets/Scripts/UI/GameButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameButton : MonoBehaviour
+{
+    public Button self;
+    public TMP_Text buttonText;
+}
