de7047a90731a6433911d1be9755c8c24b9f963f
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-07-10 23:38:33 +0900

    에셋번들 보관용 컨테이너

---
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ResourceManager
+{
+    /*public static Dictionary<string, GameObject> combinedobject = new Dictionary<string, GameObject>();
+    public static Dictionary<string, GameObject> monster = new Dictionary<string, GameObject>();
+    public static Dictionary<string, GameObject> npc = new Dictionary<string, GameObject>();
+    public static Dictionary<string, GameObject> particle = new Dictionary<string, GameObject>();
+    public static Dictionary<string, GameObject> player = new Dictionary<string, GameObject>();
+    public static Dictionary<string, GameObject> props = new Dictionary<string, GameObject>();
+    public static Dictionary<string, GameObject> rocks = new Dictionary<string, GameObject>();
+    public static Dictionary<string, GameObject> terrain = new Dictionary<string, GameObject>();
+    public static Dictionary<string, GameObject> vegetation = new Dictionary<string, GameObject>();
+    public static Dictionary<string, GameObject> weapon = new Dictionary<string, GameObject>();*/
+
+    public static AssetBundle weapon;
+    public static AssetBundle vegetation;
+    public static AssetBundle terrain;
+    public static AssetBundle rocks;
+    public static AssetBundle props;
+    public static AssetBundle player;
+    public static AssetBundle particle;
+    public static AssetBundle npc;
+    public static AssetBundle monster;
+    public static AssetBundle combinedobject;
+}
