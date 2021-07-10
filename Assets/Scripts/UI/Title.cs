834261d1f5bc625ac691efc4cb23c854d143eb8a
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-07-10 23:40:31 +0900

    타이틀 메뉴 관리자

--- Assets/Scripts/UI/Title.cs
+++ Assets/Scripts/UI/Title.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Title : MonoBehaviour
 {
+    public Image titleBackground;
+    public Image titleImage;
+
+    public Image buttonBackground;
+    public GameButton bStart;
+    public GameButton bQuit;
+
     public void GameQuit()
     {
         Application.Quit();
@@ -14,5 +22,13 @@ public class Title : MonoBehaviour
     public void GameStart()
     {
         SceneManager.LoadScene((int)SceneType.Village);
+    }
+
+    public void ButtonFadeOut()
+    {
+        buttonBackground.gameObject.SetActive(true);
+        StartCoroutine(Utility.AlphaBlending(buttonBackground, 1, 1f));
+        StartCoroutine(Utility.AlphaBlending(bStart.buttonText, 1, 1f));
+        StartCoroutine(Utility.AlphaBlending(bQuit.buttonText, 1, 1f));
     }
 }
