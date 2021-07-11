0a46c3f692e4c838d27afb3279e99245b3fbdf63
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-07-11 22:32:29 +0900

    딕셔너리로 보관할 필요없음

--- Assets/Scripts/Manager/ResourceManager.cs
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public static class ResourceManager
 {
-    /*public static Dictionary<string, GameObject> combinedobject = new Dictionary<string, GameObject>();
-    public static Dictionary<string, GameObject> monster = new Dictionary<string, GameObject>();
-    public static Dictionary<string, GameObject> npc = new Dictionary<string, GameObject>();
-    public static Dictionary<string, GameObject> particle = new Dictionary<string, GameObject>();
-    public static Dictionary<string, GameObject> player = new Dictionary<string, GameObject>();
-    public static Dictionary<string, GameObject> props = new Dictionary<string, GameObject>();
-    public static Dictionary<string, GameObject> rocks = new Dictionary<string, GameObject>();
-    public static Dictionary<string, GameObject> terrain = new Dictionary<string, GameObject>();
-    public static Dictionary<string, GameObject> vegetation = new Dictionary<string, GameObject>();
-    public static Dictionary<string, GameObject> weapon = new Dictionary<string, GameObject>();*/
-
     public static AssetBundle weapon;
     public static AssetBundle vegetation;
     public static AssetBundle terrain;
