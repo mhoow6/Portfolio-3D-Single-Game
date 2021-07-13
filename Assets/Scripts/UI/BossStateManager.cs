dcd696f9b93ac2b15a6ae3c18d3ca37eebcac1c5
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-07-13 22:18:38 +0900

    드래곤이 죽으면 페이드 아웃 되면서 브금 OFF

--- Assets/Scripts/UI/BossStateManager.cs
+++ Assets/Scripts/UI/BossStateManager.cs
@@ -23,6 +23,20 @@ public class BossStateManager : MonoBehaviour
         bossSlider.value = bossHp;
         bossSlider.maxValue = bossTotalHp;
         txtbBossTotalHp.text = bossTotalHp.ToString();
+
+        GameManager.instance.BGM = AudioManager.instance.PlayAudio(AudioManager.instance.GetAudio(AudioCondition.SCENE_FOREST, AudioCondition.DRAGON_BATTLE_START));
+        GameManager.instance.BGM.loop = true;
+        AudioManager.instance.PlayAudioFadeIn(GameManager.instance.BGM, AudioManager.instance._DRAGON_BATTLE_SOUND);
+    }
+
+    private void OnDisable()
+    {
+        if (GameManager.instance.BGM != null)
+        {
+            AudioManager.instance.StopAudioFadeOut(GameManager.instance.BGM);
+            Invoke("DeadAfter", 1f);
+        }
+            
     }
 
     private void Update()
@@ -30,5 +44,12 @@ public class BossStateManager : MonoBehaviour
         bossHp = GameManager.instance.bossCam.boss.hp;
         txtBossHp.text = bossHp.ToString();
         bossSlider.value = bossHp;
+    }
+
+    private void DeadAfter()
+    {
+        GameManager.instance.BGM = AudioManager.instance.PlayAudio(AudioManager.instance.GetAudio(AudioCondition.SCENE_FOREST, AudioCondition.SCENE_AWAKE));
+        GameManager.instance.BGM.loop = true;
+        AudioManager.instance.PlayAudioFadeIn(GameManager.instance.BGM, AudioManager.instance._FOREST_SOUND);
     }
 }
