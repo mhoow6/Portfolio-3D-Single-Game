ac0a1525949ecf5db8e77bd40ee95620a0a9f17e
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-07-13 22:09:02 +0900

    agent가 켜질때만 이동 및 공격을 하도록 함

--- Assets/Scripts/Animation/Monster/CommonMonster/CommonMonsterWalk.cs
+++ Assets/Scripts/Animation/Monster/CommonMonster/CommonMonsterWalk.cs
@@ -6,17 +6,21 @@ public class CommonMonsterWalk : CommonMonsterAnimation
 {
     protected override void StateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        self.agent.speed = self.walk_speed;
-        self.agent.acceleration = self.walk_speed;
+        if (self.agent.enabled == true)
+        {
+            self.agent.speed = self.walk_speed;
+            self.agent.acceleration = self.walk_speed;
+        }
     }
 
     protected override void StateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if (self.agent.enabled == true)
+        {
             self.agent.destination = GameManager.instance.controller.player.transform.position;
-
-        if (self.thinking_param == (int)AniType.ATTACK)
-            self.agent.destination = self.transform.position;
+            if (self.thinking_param == (int)AniType.ATTACK)
+                self.agent.destination = self.transform.position;
+        }
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
