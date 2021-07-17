9d9bef6ab6da40f0b9295b600d31931057f8263d
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-07-17 11:49:02 +0900

    어택 시작 시에 쿨다운 플래그를 키게 변경

--- Assets/Scripts/Animation/Monster/EliteMonster/EliteMonsterAttack.cs
+++ Assets/Scripts/Animation/Monster/EliteMonster/EliteMonsterAttack.cs
@@ -9,16 +9,14 @@ public class EliteMonsterAttack : EliteMonsterAnimation
         animationHandler = IdleCondition;
         animationHandler += InjuredCondition;
         animationHandler += DeadCondition;
+        self.isAttackCooldown = true;
     }
 
     protected override void StateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if (currentAnimationTime >= (1 - animationTransitionTime))
-        {
-            self.isAttackCooldown = true;
             self.thinking_param = (int)AniType.IDLE;
-        }
-            
+
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- Assets/Scripts/Animation/Monster/EliteMonster/EliteMonsterSkill_01.cs
+++ Assets/Scripts/Animation/Monster/EliteMonster/EliteMonsterSkill_01.cs
@@ -9,16 +9,13 @@ public class EliteMonsterSkill_01 : EliteMonsterAnimation
         animationHandler = IdleCondition;
         animationHandler += InjuredCondition;
         animationHandler += DeadCondition;
+        self.isAttackCooldown = true;
     }
 
     protected override void StateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if (currentAnimationTime >= (1 - animationTransitionTime))
-        {
-            self.isAttackCooldown = true;
             self.thinking_param = (int)AniType.IDLE;
-        }
-
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
