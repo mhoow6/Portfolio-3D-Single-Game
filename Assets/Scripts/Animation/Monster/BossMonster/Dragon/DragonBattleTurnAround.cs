5133e4fbe56e35c002861a170344bb0396f589af
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-07-17 11:49:15 +0900

    드래곤이 도는 와중에도 죽을 수 있다.

--- Assets/Scripts/Animation/Monster/BossMonster/Dragon/DragonBattleTurnAround.cs
+++ Assets/Scripts/Animation/Monster/BossMonster/Dragon/DragonBattleTurnAround.cs
@@ -6,6 +6,8 @@ public class DragonBattleTurnAround : DragonAnimation
 {
     protected override void StateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        aniHandler = DeadCondition;
+
         self.TurnAround(self._TURN_AROUND_SPEED);
 
         if (currentAnimationTime >= (1 - defaultAnimationTransitionTime))
