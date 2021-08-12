6a1642b3a2160cbdae377fd7ea839e3b928d6ac4
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-08-12 21:59:51 +0900

    (서버용) 클라이언트 전송에 필요할 파일을 담아두는 컨테이너

---
+++ Assets/Scripts/Network/File/FileContainer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ServerCore
+{
+    public class FileContainer
+    {
+        public static FileContainer Instance { get; } = new FileContainer();
+
+        string root;
+        Dictionary<string, byte[]> files = new Dictionary<string, byte[]>();
+        const int MAX_SEND_DATA = 65000;
+
+
+        // C:\Users\mhoow\AppData\LocalLow\JWY\AssetBundles
+        public FileContainer(string _root = "C:\\Users\\mhoow\\AppData\\LocalLow\\JWY\\AssetBundles")
+        {
+            root = _root;
+            ReadChildrenFile();
+        }
+
+        public ArraySegment<byte>? GetFile(int seek, string fileName)
+        {
+            if (files.TryGetValue(fileName, out _))
+            {
+                int seekLength = files[fileName].Length - seek > MAX_SEND_DATA ? MAX_SEND_DATA : files[fileName].Length - seek;
+                
+                return new ArraySegment<byte>(files[fileName], seek, seekLength);
+            }
+
+            return null;
+        }
+
+        void ReadChildrenFile()
+        {
+            if (Directory.Exists(root))
+            {
+                string[] filePaths = Directory.GetFiles(root);
+                char separator = '\\';
+
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    string[] splitFileName = filePaths[i].Split(separator);
+                    string fileName = splitFileName[splitFileName.Length - 1];
+
+                    if (fileName != null)
+                        files.Add(fileName, File.ReadAllBytes(filePaths[i]));
+                }
+            }                
+        }
+    }
+}
