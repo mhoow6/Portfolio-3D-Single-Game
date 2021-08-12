98036b4a425a34a781b3c445d9d291376d08d086
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-08-12 21:58:18 +0900

    네트워크 통신시에 처리할 패킷(버퍼)을 큐에 담을 자료구조

---
+++ Assets/Scripts/Network/PacketQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    public class PacketQueue
+    {
+        Queue<ArraySegment<byte>> buffQueue = new Queue<ArraySegment<byte>>();
+        object _lock = new object();
+
+        public void Push(ArraySegment<byte> buff)
+        {
+            lock (_lock)
+            {
+                buffQueue.Enqueue(buff);
+            }
+        }
+
+        public ArraySegment<byte>? Pop()
+        {
+            lock (_lock)
+            {
+                if (buffQueue.Count == 0)
+                    return null;
+
+                return buffQueue.Dequeue();
+            }
+        }
+
+        public List<ArraySegment<byte>> PopAll()
+        {
+            List<ArraySegment<byte>> list = new List<ArraySegment<byte>>();
+
+            lock (_lock)
+            {
+                while (buffQueue.Count > 0)
+                    list.Add(buffQueue.Dequeue());
+            }
+
+            return list;
+        }
+    }
+}
